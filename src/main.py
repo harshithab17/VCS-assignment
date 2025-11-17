4d5a56650f467f3b3096308b7b5c1a463b0230d5
Harshitha <harshitha@gmail.com>
2025-11-17 23:35:48 +0530

    Revert "Add temporary debug line"
    This reverts commit 319152f44b6d74e4fdd805700bf05b6c1caa3c9d.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ def greet():
     print("Hello! This is my Phase 1 Git project file.")
     print("Final merged UI version")
     print("This line is for PR demonstration")
-    print("TEMP: debug line to remove")
+
+tasks = ["Study VCS", "Do assignment", "Push to GitHub"]
+print("Today's tasks:", tasks)
 
 if __name__ == "__main__":
     greet()
