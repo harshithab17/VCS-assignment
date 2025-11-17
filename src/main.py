4ef8facdcbbeb9c5cd8cc5df8e6276530d9abd42
Harshitha <harshitha@gmail.com>
2025-11-17 22:42:33 +0530

    Revert 'Add temporary debug print'

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 def greet():
     print("Hello! This is my Phase 1 Git project file.")
     print("Final merged UI version")
-    print("TEMP: debug line to remove")
 
 if __name__ == "__main__":
     greet()
