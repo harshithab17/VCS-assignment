3833875209b231b4e5200676bc99ccf6ac3a87bb
Harshitha <harshitha@gmail.com>
2025-11-17 23:07:30 +0530

    Add PR demo print line

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 def greet():
     print("Hello! This is my Phase 1 Git project file.")
     print("Final merged UI version")
+    print("This line is for PR demonstration")
 
 if __name__ == "__main__":
     greet()
